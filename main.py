313d4f8aab148a575a765e827199762729e0035f
b98154435-boop <b98154435@gmail.com>
2026-05-27 09:07:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from aiogram.types import (
 # ──────────────────────────────────────────
 # ⚙️ KONFIGURATSIYA (UZUM MARKET PRO)
 # ──────────────────────────────────────────
-BOT_TOKEN = "8747604242:AAEyzcOcHOM5fPwBVRK4pl46T2EIGfaG5n4"
+BOT_TOKEN = "8747604242:AAGnemiFuzhy_DC7s2cbLvBiA66mtGLGeT0"
 ADMINS = [8488028783]  # Asosiy admin
 
 DB_URL = "postgresql://neondb_owner:npg_93widXgyhepO@ep-dawn-resonance-aq5a93o7-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require"
